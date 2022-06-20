a190ed665b07c3b66b8226dc7ba6d4c880702b1a
Jet <jet0jlh@gmx.de>
2022-06-20 18:45:10 +0200

    add prepareInstallZip.sh for fast creation of a new release

---
+++ prepareInstallZip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path/.."
+
+rm -r install/
+rm install.zip
+
+mkdir install
+cp -r piScreen/defaults/ install/
+cp -r piScreen/home/ install/
+cp -r piScreen/srv/ install/
+cp -r piScreen/etc/ install/
+cp -r piScreen/install.py install/
+cp -r piScreen/LICENSE install/
+rm -r install/home/pi/piScreen/certs
+rm -r install/home/pi/piScreen/settings.json
+rm -r install/home/pi/piScreen/cron.json
+zip -q -r ./install.zip ./install/
