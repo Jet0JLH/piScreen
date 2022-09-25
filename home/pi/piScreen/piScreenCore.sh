f3bd140d37f9cd027b1b584832fd56dba6655a49
Jet0JLH <jet0jlh@gmx.de>
2022-09-25 20:22:08 +0200

    Implements piScreenDisplay.sh controlfiles

--- home/pi/piScreen/piScreenCore.sh
+++ home/pi/piScreen/piScreenCore.sh
@@ -3,6 +3,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 touch /media/ramdisk/piScreenDisplayOn
 touch /media/ramdisk/piScreenDisplaySwitch
+touch /media/ramdisk/piScreenDisplayCEC
 sleep 10
 export DISPLAY=:0
 browser=firefox-esr
