d803ace6a5e5bd03908104223fb215af6d29a6c5
Jet0JLH <jet0jlh@gmx.de>
2022-09-27 21:03:26 +0200

    Add process watchdog in core

--- home/pi/piScreen/piScreenCore.sh
+++ home/pi/piScreen/piScreenCore.sh
@@ -7,19 +7,32 @@ touch /media/ramdisk/piScreenDisplayCEC
 sleep 10
 export DISPLAY=:0
 browser=firefox-esr
-processID=$(pgrep -x $browser)
+browserPID=$(pgrep -x $browser)
 
 unclutter -idle 5 &
 ./piScreenDisplay.sh &
+displayPID=$!
 ./piScreenScreenshot.py &
+screenshotPID=$!
 
 while [ true ] ; do
-	kill -0 $processID
+	kill -0 $browserPID
 	if [ $? -gt 0 ] ; then
-		echo Kein Browserprozess aktiv
 		./piScreenCmd.py --start-browser
 		sleep 5
-		processID=$(pgrep -x $browser)
+		browserPID=$(pgrep -x $browser)
+	fi
+	kill -0 $displayPID
+	if [ $? -gt 0 ] ; then
+		./piScreenDisplay.sh &
+		displayPID=$!
+		sleep 5
+	fi
+	kill -0 $screenshotPID
+	if [ $? -gt 0 ] ; then
+		./piScreenScreenshot.py &
+		screenshotPID=$!
+		sleep 5
 	fi
 	sleep 1
 done
--- home/pi/piScreen/piScreenDisplay.sh
+++ home/pi/piScreen/piScreenDisplay.sh
@@ -7,7 +7,7 @@ displaySwitchChannel=$ramdisk"piScreenDisplaySwitch"
 displayCEC=$ramdisk"piScreenDisplayCEC"
 displayDDC=$ramdisk"piScreenDisplayDDC"
 if [ -f $displayCEC ]; then
-	(while true ; do sleep 5 ; if [ ! -f $displayCec ] ; then sudo killall cec-client ; elif [ -f $displayOn ] ; then sudo rm $displayOn ; echo "on 0" ; elif [ -f $displayStandby ] ; then sudo rm $displayStandby ; echo "standby 0" ; elif [ -f $displayOff ] ; then sudo rm $displayOff ; echo "off 0" ; elif [ -f $displaySwitchChannel ] ; then sudo rm $displaySwitchChannel ; echo "as 0" ; fi ; echo "pow 0" ; done) | cec-client -d 8 -p 1 -b 5 -t p | grep power  --line-buffered | while read x ; do
+	(while true ; do sleep 5 ; if [ ! -f $displayCEC ] ; then killall cec-client ; elif [ -f $displayOn ] ; then sudo rm $displayOn ; echo "on 0" ; elif [ -f $displayStandby ] ; then sudo rm $displayStandby ; echo "standby 0" ; elif [ -f $displayOff ] ; then sudo rm $displayOff ; echo "off 0" ; elif [ -f $displaySwitchChannel ] ; then sudo rm $displaySwitchChannel ; echo "as 0" ; fi ; echo "pow 0" ; done) | cec-client -d 8 -p 1 -b 5 -t p | grep power  --line-buffered | while read x ; do
 		echo $x | awk '{split($0,a,":");print a[2]}' | sed 's/ //' | tee $displayStatus > /dev/null
 	done
 elif [ -f $displayDDC ]; then
